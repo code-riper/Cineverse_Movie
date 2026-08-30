636fd64a74b48bd880d3f5373fb5c014818eba9f
code-riper <truthincode3@gmail.com>
2026-08-30 19:08:40 +0530

    Fix production build

--- src/main.jsx
+++ src/main.jsx
@@ -15,27 +15,3 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   </React.StrictMode>
 );
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-// import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
-// import App from './App.jsx'
-
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>
-// )
